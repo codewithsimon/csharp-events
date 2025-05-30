b8444303b1fcf1d6a7cbd4620d2eee5d2876884d
Stephen SIMON <68376360+codewithsimon@users.noreply.github.com>
2025-05-30 19:58:02 +0530

    Remove CSharp TV logo from navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,6 @@ const Header: React.FC = () => {
           <div className="flex items-center">
             <img src="/logo 3.png" alt="C# Corner Logo" className="h-8 w-auto" />
           </div>
-          
-          <a 
-            href="https://csharp.tv" 
-            target="_blank"
-            rel="noopener noreferrer" 
-            className="flex items-center"
-          >
-            <img 
-              src="/csharp-tv-logo-navbar-black.png" 
-              alt="CSharp TV" 
-              className="h-6 w-auto hover:opacity-80 transition-opacity duration-300"
-            />
-          </a>
         </div>
       </div>
     </header>
